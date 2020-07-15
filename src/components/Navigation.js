423e8c4122f18fe37a9b86f1746775daa169c2a5
Drew Olsen <drew@drewolsendesign.com>
2020-07-15 02:07:03 -0500

    Added the navbar shading code to this file where it belongs

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,61 +1,85 @@
-import React from 'react';
-import { MdHome, MdFace, MdCardTravel, MdDescription } from 'react-icons/md';
-import { Link } from 'gatsby';
+import React from "react"
+import { MdHome, MdFace, MdCardTravel, MdDescription } from "react-icons/md"
+import { Link } from "gatsby"
 
-import SiteLogo from './SiteLogo';
+import SiteLogo from "./SiteLogo"
 
-const isCurrent = (anchor, pathname) => (pathname.endsWith(anchor) ? 'current' : '');
-class Navigation extends React.Component {
-	state = {
-		pathname : '#home'
-	};
-	componentDidMount() {
-		window.addEventListener('hashchange', () => {
-			this.setState({ pathname: window.location.href });
-		});
-	}
-	render() {
-		const { pathname = '' } = this.state;
-		return (
-			<nav id="nav-wrap">
-				<SiteLogo />
-				<ul id="nav" className="nav">
-					<li className={isCurrent('#home', pathname)}>
-						<Link to={`#home`}>
-							<span>
-								<MdHome />
-							</span>
-							Home
-						</Link>
-					</li>
-					<li className={isCurrent('#about', pathname)}>
-						<a className="smoothscroll" href="#about">
-							<span>
-								<MdFace />
-							</span>
-							About
-						</a>
-					</li>
-					<li className={isCurrent('#resume', pathname)}>
-						<a className="smoothscroll resume" rel="noopener" href="/downloads/DrewOlsenResume.pdf">
-							<span>
-								<MdDescription />
-							</span>
-							Resumé
-						</a>
-					</li>
-					<li className={isCurrent('#portfolio', pathname)}>
-						<a href="#portfolio" className="smoothscroll">
-							<span>
-								<MdCardTravel />
-							</span>
-							Portfolio
-						</a>
-					</li>
-				</ul>
-			</nav>
-		);
-	}
+//Lets add the class to add shading to the nav when scrolling
+if (typeof window !== `undefined`) {
+  window.addEventListener("scroll", addNavBkg)
 }
 
-export default Navigation;
+function addNavBkg(ev) {
+  const navBar = document.getElementById("nav-wrap")
+  let windowWidth = window.innerWidth
+  let scrollFromTop = window.scrollY
+  if (windowWidth >= 768 && scrollFromTop >= 130) {
+    navBar.classList.add("scroll")
+  } else {
+    if (navBar.classList.contains("scroll")) {
+      navBar.classList.remove("scroll")
+    }
+  }
+}
+
+const isCurrent = (anchor, pathname) =>
+  pathname.endsWith(anchor) ? "current" : ""
+
+class Navigation extends React.Component {
+  state = {
+    pathname: "#home",
+  }
+  componentDidMount() {
+    window.addEventListener("hashchange", () => {
+      this.setState({ pathname: window.location.href })
+    })
+  }
+  render() {
+    const { pathname = "" } = this.state
+    return (
+      <nav id="nav-wrap">
+        <SiteLogo />
+        <ul id="nav" className="nav">
+          <li className={isCurrent("#home", pathname)}>
+            <Link to={`#home`}>
+              <span>
+                <MdHome />
+              </span>
+              Home
+            </Link>
+          </li>
+          <li className={isCurrent("#about", pathname)}>
+            <a className="smoothscroll" href="#about">
+              <span>
+                <MdFace />
+              </span>
+              About
+            </a>
+          </li>
+          <li className={isCurrent("#resume", pathname)}>
+            <a
+              className="smoothscroll resume"
+              rel="noopener"
+              href="/downloads/DrewOlsenResume.pdf"
+            >
+              <span>
+                <MdDescription />
+              </span>
+              Resumé
+            </a>
+          </li>
+          <li className={isCurrent("#portfolio", pathname)}>
+            <a href="#portfolio" className="smoothscroll">
+              <span>
+                <MdCardTravel />
+              </span>
+              Portfolio
+            </a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navigation
