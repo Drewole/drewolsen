4eef554171dcfef212570f4970e9aa9ff1d6e964
Drew Olsen <drew@drewolsendesign.com>
2020-07-15 01:04:18 -0500

    Updated the 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,12 @@ import Header from "../components/Header"
 import About from "../components/About"
 import Projects from "../components/Projects"
 import Footer from "../components/Footer"
-// import Resume from '../components/Resume';
-// import Testimonials from '../components/Testimonials';
 
 const IndexPage = () => {
   return (
     <Layout>
       <Header />
       <About />
-      {/* <Resume /> */}
-      {/* <Testimonials /> */}
       <Projects />
       <Footer />
     </Layout>
