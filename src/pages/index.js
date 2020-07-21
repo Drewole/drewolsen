9789eb53ea1d66dde2b02a26ee203b04a8513980
Drew Olsen <drew@drewolsendesign.com>
2020-07-21 17:40:07 -0500

    Temp removed failing component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import Layout from '../layouts/Layout';
-import Header from '../components/Header';
+
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Footer from '../components/Footer';
 
 const IndexPage = () => (
 	<Layout>
-		<Header />
 		<About />
 		<Projects />
 		<Footer />
