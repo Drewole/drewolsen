cc6d71c654bafe8846554c01f00fc33f67f017e3
Drew Olsen <drew@drewolsendesign.com>
2020-07-21 17:50:53 -0500

    Troubleshooting netlify build issue

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,7 @@
 import React from 'react';
-
 import Navigation from './Navigation';
 import Banner from './Banner';
 
-const Header = () => (
-	<header id="home">
-		<Navigation />
-		<Banner />
-	</header>
-);
+const Header = () => <header id="home" />;
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Layout from '../layouts/Layout';
-
+import Header from '../components/Header';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Footer from '../components/Footer';
 
 const IndexPage = () => (
 	<Layout>
+		<Header />
 		<About />
 		<Projects />
 		<Footer />
