11163955302d92e0b12c509600e49a54d0bab053
Drew Olsen <drew@drewolsendesign.com>
2020-07-21 16:59:59 -0500

    Started a contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../layouts/Layout"
-import Header from "../components/Header"
-import About from "../components/About"
-import Projects from "../components/Projects"
-import Footer from "../components/Footer"
+import Layout from '../layouts/Layout';
+import Header from '../components/Header';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Footer from '../components/Footer';
 
-const IndexPage = () => {
-  return (
-    <Layout>
-      <Header />
-      <About />
-      <Projects />
-      <Footer />
-    </Layout>
-  )
-}
+const IndexPage = () => (
+	<Layout>
+		<Header />
+		<About />
+		<Projects />
+		<Footer />
+	</Layout>
+);
 
-export default IndexPage
+export default IndexPage;
