77c01f608925a12eefd234c8f2705d1da4947d4a
Drew Olsen <drew@drewolsendesign.com>
2020-08-01 02:35:21 -0500

    Additional portfolio changes

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+const path = require(`path`)
+
+module.exports = {
+  siteMetadata: {
+    title:
+      "Drew Olsen - A Designer that can build websites / web apps from the ground up.",
+
+    author: "Drew Olsen",
+    description:
+      "A Multi-faceted Front-End Web Developer with design skills... And an unhealthy fondness for Montserrat. :D",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: path.join(__dirname, `data`),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`300`, `400`, `500`, `700`, `800`],
+          },
+        ],
+      },
+    },
+    `gatsby-transformer-json`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify`,
+    `gatsby-transformer-json`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-smoothscroll`,
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import SocialLinks from './SocialLinks';
+import { FaChevronCircleUp } from 'react-icons/fa';
+import { RiGatsbyLine } from 'react-icons/ri';
+
+const Footer = () => (
+	<footer id="footer">
+		<div className="row">
+			<div className="twelve columns">
+				<SocialLinks />
+
+				<ul className="copyright">
+					<li>&copy; Copyright 2019 DrewOlsen.Design</li>
+					<li>
+						Built with{' '}
+						<a href="https://www.gatsbyjs.org/">
+							<span>
+								<RiGatsbyLine />
+							</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+
+			<div id="go-top">
+				<a className="smoothscroll" title="Back to Top" href="#home">
+					<FaChevronCircleUp />
+				</a>
+			</div>
+		</div>
+	</footer>
+);
+
+export default Footer;
