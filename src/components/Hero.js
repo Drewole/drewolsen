3ca484a910ebc3c31c5c000ce199da646a787ab6
Drew Olsen <drew@drewolsendesign.com>
2020-08-03 12:49:01 -0500

    Small fixes

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './Navigation';
+import BannerImage from './BannerImage';
+
+const Hero = () => (
+	<header id="home">
+		<Navigation />
+		<BannerImage />
+	</header>
+);
+
+export default Hero;
