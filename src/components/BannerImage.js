7d4a0eba22512ab7b1ac64fb6ac5fea0a32f5539
Drew Olsen <Drewole@users.noreply.github.com>
2020-08-05 12:05:03 -0500

    Headline Change

--- src/components/BannerImage.js
+++ src/components/BannerImage.js
@@ -32,7 +32,7 @@ const BannerImage = () => (
         >
           <div className="row banner">
             <div className="banner-text">
-              <h1 className="responsive-headline">A Designer That Can Code.</h1>
+              <h1 className="responsive-headline">l Make Useful S*@~t.</h1>
               <h3>
                 A Multi-faceted Front-End Web Developer with a knack for
                 effectively conceptualizing and delivering elegant,
